715cb63d869bd158952414cd540efe2b2bc50408
johnfn <johnfn@gmail.com>
2023-04-29 21:46:40 -0700

    I am a college freshman.

--- Globals.cs
+++ Globals.cs
@@ -8,14 +8,14 @@ public enum LayerMask {
 
 public static class Globals {
   public static Dictionary<uint, LayerMask> LayerMasks = new() {
-    [0 << 1] = LayerMask.Wall,
+    [1 << 0] = LayerMask.Wall,
     [1 << 1] = LayerMask.Spikes,
-    [2 << 1] = LayerMask.Cannonball
+    [1 << 2] = LayerMask.Cannonball
   };
 
   public static Dictionary<LayerMask, int> LayerNumbers = new() {
-    [LayerMask.Wall] = 0 << 1,
+    [LayerMask.Wall] = 1 << 0,
     [LayerMask.Spikes] = 1 << 1,
-    [LayerMask.Cannonball] = 2 << 1
+    [LayerMask.Cannonball] = 1 << 2
   };
 }
